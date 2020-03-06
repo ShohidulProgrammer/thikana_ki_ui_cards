57d316be243280610e59aee64794c95aa1ea9809
Shohidul Islam <shoikat.cse@gmail.com>
2020-03-06 11:11:58 +0600

    init thikana ki app

---
+++ android/app/src/main/kotlin/com/bksoft/thikana_ki/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.bksoft.thikana_ki
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}

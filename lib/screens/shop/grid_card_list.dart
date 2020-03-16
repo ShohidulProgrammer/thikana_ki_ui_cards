c59a671e76e95c4efcddbf42b61ea1956329fa7c
Shohidul Islam <shoikat.cse@gmail.com>
2020-03-16 16:17:05 +0600

    resize grid card view

--- lib/screens/shop/grid_card_list.dart
+++ lib/screens/shop/grid_card_list.dart
@@ -3,16 +3,21 @@ import 'package:thikana_ki/models/screen_models/shop_model.dart';
 import 'package:thikana_ki/widgets/shop/SmallCard.dart';
 
 class Shop extends StatelessWidget {
+  final double itemHeight = 200;
+  final double itemWidth = 150;
+
   @override
   Widget build(BuildContext context) {
     final title = "Discount Shop";
 
     return GridView.count(
         crossAxisCount: 3,
+        childAspectRatio: (itemWidth / itemHeight),
         children: List.generate(choices.length, (index) {
-          return Center(
-            child: SmallCard(shopModel: choices[index]),
-          );
+          return Container(
+
+              color: Colors.grey[100],
+              child: SmallCard(shopModel: choices[index]));
         }));
   }
 }
--- lib/widgets/shop/SmallCard.dart
+++ lib/widgets/shop/SmallCard.dart
@@ -8,48 +8,50 @@ class SmallCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final TextStyle textStyle = Theme.of(context).textTheme.display1;
-    return Container(
-      height: 5000,
-      child: Card(
-//        semanticContainer: true,
-        clipBehavior: Clip.antiAliasWithSaveLayer,
-        child: InkWell(
-          onTap: () {
-            print("Card Item was clicked");
-          },
-          child: Center(
-            child: Column(
-              children: <Widget>[
-                Expanded(
-                  flex: 2,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 5.0, horizontal: 20.0),
-                    child: Image.network(
-                      'https://dlior9lx1k7r2.cloudfront.net/companylogo/9542879f-ca20-42e5-88b8-421faa7befb7.jpg',
-                      // width: 300,
+
+    return Card(
+      semanticContainer: true,
+      clipBehavior: Clip.antiAliasWithSaveLayer,
+      child: InkWell(
+        splashColor: Colors.blue.withAlpha(30),
+        onTap: () {
+          print("Card Item was clicked");
+        },
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Expanded(
+                flex: 2,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 10.0, horizontal: 20.0),
+                  child: Image.network(
+                    'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS9vAP24XvNRDnkwgwUmN72s4DnitZMjMHFh22U4bAf30PZu1p3',
+                    // width: 300,
 //                  height: 100,
-                      fit: BoxFit.contain,
-                    ),
+                    fit: BoxFit.contain,
                   ),
                 ),
-                Expanded(
-                  flex: 1,
+              ),
+              Expanded(
+                flex: 1,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
                   child: Text(
                     shopModel.title,
 //                  style: textStyle
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10.0),
-        ),
-        elevation: 5,
-        margin: EdgeInsets.all(5),
       ),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+//        elevation: 5,
+//        margin: EdgeInsets.all(5),
     );
   }
 }
